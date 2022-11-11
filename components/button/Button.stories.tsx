0773538bec5426dbb676ddab681baa71884bbce5
Mohamed Saleh <msaleh.eg87@gmail.com>
2022-11-12 00:47:52 +0200

    update button label in storybook

--- components/button/Button.stories.tsx
+++ components/button/Button.stories.tsx
@@ -20,7 +20,7 @@ export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   primary: true,
-  label: 'Button',
+  label: 'THE TEST BUTTON',
 };
 
 export const Secondary = Template.bind({});
